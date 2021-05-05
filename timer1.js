03dedad0dbc802fde7a0841f2cb3d13d25abc706
CDNDragoon <tjgossage@gmail.com>
2021-05-05 00:56:48 +0000

    made a timer

--- timer1.js
+++ timer1.js
@@ -1,1 +1,12 @@
-//test
+const listOfTimes = process.argv.slice(2);
+
+const timer = (timers) => {
+  for (let element of timers) {
+    setTimeout(function() {
+      process.stdout.write('\x07');
+      console.log('beep');
+    }, element * 1000);
+  }
+};
+
+timer(listOfTimes);
